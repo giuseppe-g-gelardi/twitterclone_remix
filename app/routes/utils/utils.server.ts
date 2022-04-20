9d4876727bce32f381d9f401d5b08157fe54b790
n-rdy <giuseppegelardi@icloud.com>
2022-04-20 14:55:00 -0400

    install and setup cloudinary, updated .env, add test route for image and local uploading

---
+++ app/routes/utils/utils.server.ts
@@ -0,0 +1,28 @@
+import cloudinary from 'cloudinary'
+import type { Stream } from 'stream'
+
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET_KEY,
+});
+
+async function uploadImage(fileStream: Stream) {
+  return new Promise((resolve, reject) => {
+    const uploadStream = cloudinary.v2.uploader.upload_stream(
+      {
+        folder: "remix",
+      },
+      (error, result) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(result);
+      }
+    );
+    fileStream.pipe(uploadStream);
+  });
+}
+
+console.log("configs", cloudinary.v2.config());
+export { uploadImage };
