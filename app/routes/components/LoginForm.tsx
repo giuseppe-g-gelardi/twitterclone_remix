d59333bcfbc38fd0b354b9a9ca52054f6b0ddd9f
n-rdy <giuseppegelardi@icloud.com>
2022-04-26 17:35:52 -0400

    start login form validation

--- app/routes/components/LoginForm.tsx
+++ app/routes/components/LoginForm.tsx
@@ -1,15 +1,61 @@
-import { Form } from "@remix-run/react";
+import { Form, useActionData } from "@remix-run/react";
 
 export default function LoginForm() {
+  const actionData = useActionData()
+
   return (
-    <div>
-        <Form replace method='post' className="flex flex-col">
-          <input type='text' name='email' placeholder='Email' />
-          <input type='password' name='password' placeholder='Password' />
-          <button type='submit' value='login' style={{ backgroundColor: 'lightBlue' }}>
-            Login
-          </button>
-        </Form>
+    <div className="flex items-center space-y-4 py-16 font-semibold text-gray-500 flex-col">
+      <h1 className="text-slate-500 text-2xl">
+        Sign in to continue
+      </h1>
+      <Form replace method='post' className="flex flex-col">
+        <input
+          className="w-full p-2 rounded-md border border-gray-700 focus:border-blue-700"
+          type='text'
+          name='email'
+          id='email'
+          placeholder='Email'
+          // defaultValue={actionData?.fieldErrors?.email}
+        
+          //
+        />
+        <div>
+          {actionData?.fieldErrors?.email ? (
+            <p
+              className='text-red-700'
+              role='alert'
+              id='username-error'
+            >
+              {actionData?.fieldErrors?.email}
+            </p>
+          ) : null}
+
+        </div>
+        <input
+          className="w-full p-2 rounded-md border border-gray-700"
+          type='password'
+          name='password'
+          placeholder='Password'
+          id='password'
+          // defaultValue={actionData?.fieldErrors?.password}
+        />
+        <div>
+          {actionData?.fieldErrors?.password ? (
+            <p
+              className='text-red-700'
+              role='alert'
+              id='password-error'
+            >
+              {actionData?.fieldErrors?.password}
+            </p>
+          ) : null}
+        </div>
+        <button 
+          className="w-full p-2 bg-gray-50 rounded-full font-bold text-gray-900 border border-gray-700 "
+          type='submit' value='login' style={{ backgroundColor: 'lightBlue' }}>
+          Login
+        </button>
+      </Form>
     </div>
   )
 }
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,30 +1,57 @@
+import { json } from "@remix-run/node"
+import type { ActionFunction } from "@remix-run/node"
+
 import { login, createUserSession } from "./api/session.server"
 import LoginForm from "./components/LoginForm"
-import type { ActionFunction } from "@remix-run/node"
 import type { User } from "./api/models/user.models"
+
+function validateEmail(email: any) {
+  if (typeof email !== 'string' || email.length < 3) {
+    return 'Invalid Email'
+  }
+}
+
+function validatePassword(password: any) {
+  if (typeof password !== 'string' || password.length < 6) {
+    return 'Password must be at least 6 characters'
+  }
+}
+
+function badRequest(data: any) {
+  return json(data, { status: 400 })
+}
 
 export const action: ActionFunction = async ({ request }) => {
   const form = await request.formData()
   const email = form.get('email') as string
   const password = form.get('password') as string
-  const fields: {email: string, password: string } = { email, password }
+  const fields: { email: string, password: string } = { email, password }
+
+  const fieldErrors = {
+    email: validateEmail(email),
+    password: validatePassword(password),
+  }
+
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({ fieldErrors, fields })
+  }
 
   // logs in user
   const user: User = await login(fields)
+  if (!user) return badRequest({
+    fields,
+    fieldErrors: { email: 'Invalid Credentials' }
+  })
 
   // create user session and redirects to the home page
   return createUserSession(user._id, '/home')
 }
 
 export default function LoginPage() {
+
   return (
-    <>
-      <div className='page-header'>
-        <h1>Login Page</h1>
-      </div>
-      <div className='page-content'>
-        <LoginForm />
-      </div>
-    </>
+    <div>
+      <LoginForm />
+    </div>
   )
 }
