b856d1f45bed61ba680bac5dd21e1877564bf794
n-rdy <giuseppegelardi@icloud.com>
2022-05-04 11:50:03 -0400

    add forms and styles to edit profile modal

--- app/routes/api/user.server.ts
+++ app/routes/api/user.server.ts
@@ -41,7 +41,74 @@ export async function uploadProfileImage(username: any, image: any) {
   return profilePicture
 }
 
+export async function uploadProfileBanner(username: any, image: any) {
+  const response = await fetch(`http://localhost:8000/api/users/${username}/update`, {
+    method: 'PUT',
+    body: JSON.stringify({ image }),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+  const profileBanner = await response.json()
+
+  return profileBanner
+}
+
 export async function followAndUnfollowUsers() {
   return true
 }
+
+// consolidate the profile updates
+export async function updateFirstname(username: any, firstname: any) {
+  const response = await fetch(`http://localhost:8000/api/users/${username}/updateuser`, {
+    method: 'PUT',
+    body: JSON.stringify({ firstname }),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+  const update = await response.json()
+
+  return update
+}
+
+export async function updateLastname(username: any, lastname: any) {
+  const response = await fetch(`http://localhost:8000/api/users/${username}/updateuser`, {
+    method: 'PUT',
+    body: JSON.stringify({ lastname }),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+  const update = await response.json()
+
+  return update
+}
+
+export async function updateBio(username: any, bio: any) {
+  const response = await fetch(`http://localhost:8000/api/users/${username}/updateuser`, {
+    method: 'PUT',
+    body: JSON.stringify({ bio }),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+  const update = await response.json()
+
+  return update
+}
+
+export async function updateLocation(username: any, location: any) {
+  const response = await fetch(`http://localhost:8000/api/users/${username}/updateuser`, {
+    method: 'PUT',
+    body: JSON.stringify({ location }),
+    headers: {
+      'Content-type': 'application/json'
+    }
+  })
+  const update = await response.json()
+
+  return update
+}
+
 
--- app/routes/components/Modal.tsx
+++ app/routes/components/Modal.tsx
@@ -1,13 +1,39 @@
+import type { ActionFunction } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
 import { useState } from "react";
+
 import type { User } from '../api/models/user.models'
+import { getUser } from "../api/session.server";
+import { updateBio, updateFirstname, updateLastname, updateLocation } from "../api/user.server";
 
 type LoaderData = {
   loggedInUser: User
 }
 
-export default function Modal({ buttonText, header }: any) {
-  const [showModal, setShowModal] = useState(false);
+// TODO: connect the form
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData()
+  const user: User | null = await getUser(request)
+  const data = { user }
+  const loggedInUser = data.user
+  const firstname = form.get('firstname') as string
+  const lastname = form.get('lastname') as string
+  const bio = form.get('bio') as string
+  const location = form.get('location') as string
+
+  const updateFirst = await updateFirstname(loggedInUser?.username, firstname)
+  const updateLast = await updateLastname(loggedInUser?.username, lastname)
+  const update_bio = await updateBio(loggedInUser?.username, bio)
+  const update_location = await updateLocation(loggedInUser?.username, location)
+
+  return { updateFirst, updateLast, update_bio, update_location }
+}
+
+export default function Modal({ 
+  buttonText, header }: { buttonText: string, header: string 
+  }) {
+  const [showModal, setShowModal] = useState<boolean>(false);
   const { loggedInUser } = useLoaderData<LoaderData>()
   return (
     <>
@@ -20,32 +46,32 @@ export default function Modal({ buttonText, header }: any) {
       </button>
       {showModal ? (
         <>
-          <div
-            className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
-          >
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
               {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-slate-200 outline-none focus:outline-none">
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
+                  <h3 className="text-3xl font-semibold text-black">
                     {header}
                   </h3>
                   <button
-                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    className="p-1 ml-auto border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                     onClick={() => setShowModal(false)}
                   >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                    <span className=" text-red-500 text-3xl hover:bg-red-200 rounded-full">
                       ×
                     </span>
                   </button>
+
                 </div>
                 {/*body*/}
                 <div className="relative p-6 flex-auto">
-                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                    
-                    <Form>
 
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+
+                    <Form replace method='put'>
+                      {/* add profile image && banner */}
                       <input
                         className="w-full p-2 rounded-md border border-gray-700 focus:border-blue-700"
                         type='text'
@@ -62,40 +88,43 @@ export default function Modal({ buttonText, header }: any) {
                         placeholder={loggedInUser?.lastname}
                       />
 
-                      <textarea 
+                      <textarea
                         className="w-full p-2 rounded-md border border-gray-700 focus:border-blue-700"
                         name='bio'
                         id='bio'
                         placeholder={loggedInUser?.bio}
                       />
 
-                      <input 
+                      <input
                         className="w-full p-2 rounded-md border border-gray-700 focus:border-blue-700"
                         type='text'
                         name='location'
                         id='location'
                         placeholder={loggedInUser?.location}
                       />
+
+                      <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                        <button
+                          className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                          type="button"
+                          onClick={() => setShowModal(false)}
+                        >
+                          Close
+                        </button>
+                        <button
+                          className="bg-violet-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                          type="submit"
+                          onClick={() => setShowModal(false)}
+                        >
+                          Save Changes
+                        </button>
+                      </div>
                     </Form>
+
                   </p>
                 </div>
                 {/*footer*/}
-                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </button>
-                  <button
-                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Save Changes
-                  </button>
-                </div>
+
               </div>
             </div>
           </div>
