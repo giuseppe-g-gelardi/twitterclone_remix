f6e34c93c52415ccfe8e31a6004e6cd1e4f489c3
n-rdy <giuseppegelardi@icloud.com>
2022-05-04 14:26:24 -0400

    clean up modal file

--- app/routes/components/Modal.tsx
+++ app/routes/components/Modal.tsx
@@ -27,9 +27,9 @@ export default function Modal({
         <Form replace method='put'>
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              {/*content*/}
+{/*content*/}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-slate-200 outline-none focus:outline-none">
-                {/*header*/}
+{/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                   <h3 className="text-3xl font-semibold text-black">
                     {header}
@@ -43,11 +43,6 @@ export default function Modal({
                     </span>
                   </button>
                 </div>
-
-
-
-
-
 {/* profile banner and pfp */}
                 <div className="flex w-full h-52 relative bg-gray-500">
                   {loggedInUser.profileBanner ? (
@@ -68,32 +63,23 @@ export default function Modal({
                     />
                   )}
                 </div>
-                      {/* profile banner and pfp */}
-
                 <div className="flex z-10 -mt-16">
                   <button
                     onClick={() => console.log('update profile image')}
-                  >
-
+                    >
                     <img
                       src={loggedInUser.profilePicture}
                       alt=''
                       className="z-10 inline object-cover w-36 h-36 mr-2 rounded-full border-2 ml-4"
-                    />
+                      />
                   </button>
                 </div>
+{/* profile banner and pfp */}
 
-
-
-
-
-                {/*body*/}
+{/*body*/}
                 <div className="relative p-6 flex-auto">
-
                   <p className="my-4 text-slate-500 text-lg leading-relaxed">
-
                     <div id='form wrapper'>
-
                       <input
                         className="w-full p-2 rounded-md border border-gray-700 focus:border-blue-700"
                         type='text'
@@ -102,8 +88,6 @@ export default function Modal({
                         placeholder={loggedInUser?.firstname}
                         defaultValue={loggedInUser?.firstname}
                       />
-
-
                       <input
                         className="w-full p-2 rounded-md border border-gray-700 focus:border-blue-700"
                         type='text'
@@ -112,7 +96,6 @@ export default function Modal({
                         placeholder={loggedInUser?.lastname}
                         defaultValue={loggedInUser?.lastname}
                       />
-
                       <textarea
                         className="w-full p-2 rounded-md border border-gray-700 focus:border-blue-700"
                         name='bio'
@@ -120,7 +103,6 @@ export default function Modal({
                         placeholder={loggedInUser?.bio}
                         defaultValue={loggedInUser?.bio}
                       />
-
                       <input
                         className="w-full p-2 rounded-md border border-gray-700 focus:border-blue-700"
                         type='text'
@@ -129,13 +111,10 @@ export default function Modal({
                         placeholder={loggedInUser?.location}
                         defaultValue={loggedInUser?.location}
                       />
-
-
                     </div>
-
                   </p>
                 </div>
-                {/*footer*/}
+{/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                   <button
                     className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
@@ -148,7 +127,6 @@ export default function Modal({
                     type="submit"
                     name='_action'
                     value='update'
-                    // onClick={() => setShowModal(false)}
                   >
                     Save Changes
                   </button>
